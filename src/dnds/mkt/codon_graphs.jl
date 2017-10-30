dee06b6e08271b5412a033cc85a544f139aa5de0
Ben J. Ward <ward9250@gmail.com>
2017-10-30 14:42:37 +0000

    Added codon_graphs

---
+++ src/dnds/mkt/codon_graphs.jl
@@ -0,0 +1,122 @@
+
+const DSDN_RANK_LOOKUP = [0 2 5 9;
+                          1 4 8 0;
+                          3 7 0 0;
+                          6 0 0 0;]
+
+@inline function rankof(DS::Integer, DN::Integer)
+    @inbounds return DSDN_RANK_LOOKUP[DS + 1, DN + 1]
+end
+
+struct CodonGraphReference{C<:Codon}
+    edges::Vector{Tuple{C,C,Int}}
+    edge_permutation::Vector{Int}
+    edge_order::Vector{Int}
+end
+
+function CodonGraphReference{C}(code::GeneticCode) where C <: Codon
+    edgetable = make_edge_reference(code)
+    edgetable_perm = sortperm(edgetable, by = x -> x[3])
+    edgetable_order = zeros(edgetable_perm)
+    for i in eachindex(edgetable_perm)
+        perm = edgetable_perm[i]
+        edgetable_order[perm] = i
+    end
+    return CodonGraphReference{C}(edgetable, edgetable_perm, edgetable_order)
+end
+
+function make_edge_reference(code::GeneticCode)
+    edges = Vector{Tuple{DNACodon,DNACodon,Int}}(2016)
+    k = 1
+    for i in 0x00:0x3F
+        for j in (i + 1):0x3F
+            x = C(UInt64(i))
+            y = C(UInt64(j))
+            DS, DN = DS_DN_enumerator(ShortestPath, x, y, code)
+            edges[k] = (x, y, rankof(Integer(DS), Integer(DN)))
+            k += 1
+        end
+    end
+    return edges
+end
+
+@inline function ij_to_k(i::Integer, j::Integer)
+    nelements = 64
+    x = nelements - i
+    return div(nelements * (nelements-1) - (x * (x - 1)), 2) - nelements + j
+end
+
+@inline function codons_to_k(i::T, j::T) where T <: Codon
+    return ij_to_k(UInt64(i) + 1, UInt64(j) + 1)
+end
+
+@inline function lookup_edge(table::CodonGraphReference{T}, i::T, j::T) where T <: Codon
+    @inbounds return table.edges[codons_to_k(i, j)]
+end
+
+@inline function lookup_perm(table::CodonGraphReference{T}, i::T, j::T) where T <: Codon
+    @inbounds return table.edge_permutation[codons_to_k(i, j)]
+end
+
+@inline function lookup_order(table::CodonGraphReference{T}, i::T, j::T) where T <: Codon
+    @inbounds return table.edge_order[codons_to_k(i, j)]
+end
+
+const DEFAULT_CODON_GRAPH_REFERENCE = CodonGraphReference{Codon{DNA}}(DEFAULT_TRANS)
+
+mutable struct CodonGraph{C<:Codon}
+    ref::CodonGraphReference{C}
+    vertices::Vector{C}
+    edges::BitVector
+    nv::Int
+end
+
+function CodonGraph{C}(ref::CodonGraphReference{C}) where C <: Codon
+    return CodonGraph{C}(ref, Vector{C}(64), flases(2016))
+end
+
+function CodonGraph{C}(code::GeneticCode) where C <: Codon
+    reference = CodonGraphReference{C}(code)
+    return CodonGraph{C}(reference)
+end
+
+function reset!(cg::CodonGraph, cs::CodonSet)
+    # Fill the vertices buffer with codons from the codon set.
+    cg.nv = length(cs)
+    copy!(cg.vertices, cs)
+    # Reset and turn on the bits indicating which edges we have.
+    # Order of bits is not the same as cg.ref.edges, but is dictated by
+    # cg.ref.edge_order. This removes the need to sort a lot, removing a key
+    # bottleneck in Kruskal's algorithm.
+    fill!(cg.edges, false)
+    for i in 1:cg.nv
+        x = cg.vertices[i]
+        @inbounds for j in (i + 1):(cg.nv)
+            y = cg.vertices[j]
+            e = lookup_order(cg.ref, x, y)
+            cg.edges[e] = true
+        end
+    end
+end
+
+@inline Base.start(x::CodonGraph) = findnext(x, 1)
+@inline function Base.next(x::CodonGraph, state::Int)
+    @inbounds e = x.ref.edges[x.ref.edge_permutation[state]]
+    s = findnext(x, state + 1)
+    return e, s
+end
+@inline Base.done(x::CodonGraph, state::Int) = state == 0
+
+struct MSTState{C<:Codon}
+    parents::Vector{C}
+    ranks::Vector{Int}
+end
+
+function MSTState{C}() where C <: Codon
+    MSTState{C}(collect(1:64), zeros(Int, 64))
+end
+
+@inline function reset!(x::MSTState{C}) where C <: Codon
+    fill!(x.ranks, 0)
+    copy!(x.parents, 1:64)
+end
--- src/dnds/mkt/mkt.jl
+++ src/dnds/mkt/mkt.jl
@@ -7,7 +7,7 @@
 # License is MIT: https://github.com/BioJulia/BioSequences.jl/blob/master/LICENSE.md
 
 include("lookups.jl")
-include("codon_parsimony_graph.jl")
+include("codon_graphs.jl")
 
 function codondiff(sequences::Vector{Vector{Codon{DNA}}}, position::Integer)
     s = CodonSet{DNA}()
@@ -27,27 +27,54 @@ end
 
 
 
-function multi_short_path(codonset::CodonSet{T},
-                          codoncache::Vector{Codon{T}},
-                          edgecache::Vector{Tuple{Int, Codon{T}, Codon{T}}},
-                          rankref::PairwiseListMatrix) where T <: NucleicAcid
+function multi_short_path(cs::CodonSet{T}, cg::CodonGraph{Codon{T}}, msts::MSTState{Codon{T}}) where T <: NucleicAcid
+    reset!(cg, cs)
+    mst(vertices, edges)
+    mst(cg, msts)
+end
 
-    # Fill the codon cache with codons from the codon set.
-    ncodons = length(codonset)
-    copy!(codoncache, codonset)
-    nedges = div(ncodons * (ncodons - 1), 2)
+function find(v, p)
+    if p[v] != v
+        p[v] = find(v, p[v])
+    end
+    return p[v]
+end
 
-    e = 1
-    for i in 1:ncodons
-        @inbounds for j in (i + 1):ncodons
-            x = codoncache[i]
-            y = codoncache[j]
-            edgecache[e] = (lookup(rankref, x, y), x, y)
-            e += 1
+function union(v1, v2, p, r)
+    r1 = find(v1, p)
+    r2 = find(v2, p)
+    if r1 != r2
+        if r[r1] > r[r2]
+            p[r2] = r1
+        else
+            p[r1] = r2
+            if r[r1] == r[r2]
+                r[r2] += 1
+            end
         end
     end
+end
 
-    println(codoncache)
-    println(edgecache)
+function mst(V, E)
+    parents = copy(V)
+    ranks = zeros(V)
+    sort!(V)
+    for (weight, v1, v2) in E
+        if find(v1, parents) != find(v2, parents)
+            union(v1, v2, parents, ranks)
+            # add edge to tree
+        end
+    end
+    return tree
+end
 
+function mst(codongraph, state)
+    reset!(state)
+    for (v1, v2, weight) in codongraph
+        if find(v1, parents) != find(v2, parents)
+            union(v1, v2, parents, ranks)
+            # add edge to tree
+        end
+    end
+    return tree
 end
