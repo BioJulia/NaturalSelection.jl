5063986a1e5bad197481828531a0b5871c083cba
Ben J. Ward <ward9250@gmail.com>
2018-01-03 15:35:15 +0000

    fix make ng86 table functions

--- src/dnds/NG86/lookups.jl
+++ src/dnds/NG86/lookups.jl
@@ -3,14 +3,16 @@ function make_S_N_NG86_table(code::GeneticCode, k::Float64 = 1.0)
     info("Setting up S_N_NG86")
     table = CodonLookupTable{1, Tuple{Float64, Float64}}()
     f = i -> S_N_NG86(i, k, code)
-    return setuplookup!(table, f)
+    setuplookup!(table, f)
+    return table
 end
 
 function make_DS_DN_NG86_table(code::GeneticCode)
     info("Setting up DS_DN_NG86")
     table = CodonLookupTable{2, Tuple{Float64, Float64}}()
     f = (i, j) -> DS_DN_NG86(i, j, code)
-    return setuplookup!(table, f)
+    setuplookup!(table, f)
+    return table
 end
 
 info("Compiling default lookup tables for NG86 dN dS...")
